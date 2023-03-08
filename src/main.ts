4db0befe7ddc9c5c77eb55b2f1534dad360b90a7
Amayas Said Amer <asaid046@uottawa.ca>
2023-03-08 13:21:00 -0500

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import './style.css';
+
+const tabs: NodeListOf<HTMLHeadingElement> = document.querySelectorAll<HTMLHeadingElement>(".tab");
+const sections: NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>(".section");
+
+console.log(tabs);
+console.log(sections);
+
+tabs.forEach((t, i) => {
+    t.addEventListener("click", _ => {
+        // Make all sections hidden
+        sections.forEach(s => {
+            s.hidden = true;
+        });
+        // Make the correct section visible
+        sections[i].hidden = false;
+    });
+});
+
+console.log("yooo");
