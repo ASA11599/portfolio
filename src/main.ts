38dda6ede8945bef009aa2655ca62ec34337cdc4
Amayas Said Amer <asaid046@uottawa.ca>
2023-04-19 10:07:04 -0400

    Add tab markers

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,18 @@ const sections: NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>
 
 tabs.forEach((t, i) => {
     t.addEventListener("click", _ => {
+        // Remove markers on every tab
+        tabs.forEach((tab) => {
+            if (tab.innerText.startsWith(">")) {
+                tab.innerText = tab.innerText.substring(1)
+            }
+        });
         // Make all sections hidden
         sections.forEach(s => {
             s.hidden = true;
         });
         // Make the correct section visible
+        t.innerText = `>${t.innerText}`;
         sections[i].hidden = false;
     });
 });
